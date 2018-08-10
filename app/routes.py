165b0bd703db2da65983fa2c2771ac696b9c3b75
Rosemary Waghorn <flyladyhawke@gmail.com>
2018-08-10 18:30:43 +1000

    Fix some pep8 issues

--- app/routes.py
+++ app/routes.py
@@ -1,52 +1,78 @@
 from flask import render_template, url_for, flash, redirect, request, send_file
 from app import app, db
-from app.forms import TimeTrialForm, RunnerForm, TimeTrialResultForm, LoadAttending, LoadResults, LoginForm, PrintTimeTrial
+from app.forms import TimeTrialForm, RunnerForm, TimeTrialResultForm, LoadAttending, LoadResults, \
+    LoginForm, PrintTimeTrial
 from app.models import TimeTrial, Runner, TimeTrialResult
 from src.time_trial import TimeTrialSpreadsheet, TimeTrialUtils
 from werkzeug.utils import secure_filename
-from werkzeug.exceptions import HTTPException, Forbidden
+from werkzeug.exceptions import Forbidden
 from flask_login import login_required, current_user, login_user, logout_user
+import os
 import matplotlib
+# Needed to use Agg to work on heroku
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-import matplotlib.ticker as ticker
-import numpy as numpy
-import os
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    current = TimeTrial.query.all()
-    return render_template('index.html', title='Index', current=current)
+    return render_template('index.html', title='Index')
 
 
-@app.route('/time_trial', methods=['GET', 'POST'])
-def time_trial():
-    form = TimeTrialForm()
+@app.route('/runner', methods=['GET', 'POST'])
+def runner():
+    form = RunnerForm()
+    form.active.data = 1
+    form.gender.data = 'O'
+    current = Runner.query.all()
     if form.validate_on_submit():
-        model = TimeTrial()
+        model = Runner()
         form.populate_obj(model)
+        username = model.first_name + '_' + model.last_name
+        model.username = username.replace(' ', '_').lower()
+        model.level = 1
         db.session.add(model)
         db.session.commit()
         flash('Your changes have been saved.')
-    elif request.args.get('remove'):
-        tt = TimeTrial.query.filter_by(date=request.args.get('date')).first_or_404()
-        db.session.delete(tt)
-        db.session.commit()
+        return redirect(url_for('runner'))
 
-    current = TimeTrial.query.all()
     breadcrumbs = [
         {'link': url_for('index'), 'text': 'Home', 'visible': True},
-        {'text': 'Time Trials'}
+        {'text': 'Runners'}
     ]
     return render_template(
-        'time_trial.html',
-        title='Time Trial List',
+        'runner.html',
+        title='Runners',
         form=form,
         current=current,
-        tables=[{'name': 'time-trial-list'}],
+        tables=[{'name': 'runner-list'}],
+        breadcrumbs=breadcrumbs,
+    )
+
+
+@app.route('/runner/view/<username>', methods=['GET', 'POST'])
+def runner_result(username):
+    current_model = Runner.query.filter_by(username=username).first_or_404()
+    results = current_model.results.order_by(TimeTrialResult.time_trial_id.asc())
+    url = make_graph(username, results)
+    visible = current_user.is_authenticated and (current_user.level >= 2 or current_user.id == current_model.id)
+    breadcrumbs = [
+        {'link': url_for('index'), 'text': 'Home', 'visible': True},
+        {'link': url_for('runner'), 'text': 'Runners', 'visible': True},
+        {
+            'link': url_for('runner_update', username=username),
+            'text': 'Update: '+str(current_model), 'visible': visible
+        },
+        {'text': 'View: '+str(current_model)}
+    ]
+    return render_template(
+        'runner_results.html',
+        title='View Runner',
+        runner=current_model,
+        results=results,
+        tables=[{'name': 'time-trial-results-list'}],
+        url=url,
         breadcrumbs=breadcrumbs,
     )
 
@@ -78,7 +104,7 @@ def runner_update(username):
     #     elif request.method == 'GET':
     #         form_add_result.runner_id.data = current_model
     #     forms['Add Runner Result'] = form_add_result
-        breadcrumbs = [
+    breadcrumbs = [
         {'link': url_for('index'), 'text': 'Home', 'visible': True},
         {'link': url_for('runner'), 'text': 'Runners', 'visible': True},
         {'link': url_for('runner_result', username=username), 'text': 'View: '+str(current_model), 'visible': True},
@@ -95,29 +121,6 @@ def runner_update(username):
     )
 
 
-@app.route('/runner/view/<username>', methods=['GET', 'POST'])
-def runner_result(username):
-    current_model = Runner.query.filter_by(username=username).first_or_404()
-    results = current_model.results.order_by(TimeTrialResult.time_trial_id.asc())
-    url = make_graph(username, results)
-    visible = current_user.is_authenticated and (current_user.level >= 2 or current_user.id == current_model.id)
-    breadcrumbs = [
-        {'link': url_for('index'), 'text': 'Home', 'visible': True},
-        {'link': url_for('runner'), 'text': 'Runners', 'visible': True},
-        {'link': url_for('runner_update', username=username), 'text': 'Update: '+str(current_model), 'visible': visible},
-        {'text': 'View: '+str(current_model)}
-    ]
-    return render_template(
-        'runner_results.html',
-        title='View Runner',
-        runner=current_model,
-        results=results,
-        tables=[{'name': 'time-trial-results-list'}],
-        url=url,
-        breadcrumbs=breadcrumbs,
-    )
-
-
 @app.route('/runner/delete/<username>', methods=['GET', 'POST'])
 def runner_delete(username):
     current_model = Runner.query.filter_by(username=username).first_or_404()
@@ -127,33 +130,31 @@ def runner_delete(username):
     return redirect(url_for('runner'))
 
 
-@app.route('/runner', methods=['GET', 'POST'])
-def runner():
-    form = RunnerForm()
-    form.active.data = 1
-    form.gender.data = 'O'
-    current = Runner.query.all()
+@app.route('/time_trial', methods=['GET', 'POST'])
+def time_trial():
+    form = TimeTrialForm()
     if form.validate_on_submit():
-        model = Runner()
+        model = TimeTrial()
         form.populate_obj(model)
-        username = model.first_name + '_' + model.last_name
-        model.username = username.replace(' ', '_').lower()
-        model.level = 1
         db.session.add(model)
         db.session.commit()
         flash('Your changes have been saved.')
-        return redirect(url_for('runner'))
+    elif request.args.get('remove'):
+        tt = TimeTrial.query.filter_by(date=request.args.get('date')).first_or_404()
+        db.session.delete(tt)
+        db.session.commit()
 
+    current = TimeTrial.query.order_by(TimeTrial.date.desc()).all()
     breadcrumbs = [
         {'link': url_for('index'), 'text': 'Home', 'visible': True},
-        {'text': 'Runners'}
+        {'text': 'Time Trials'}
     ]
     return render_template(
-        'runner.html',
-        title='Runners',
+        'time_trial.html',
+        title='Time Trial List',
         form=form,
         current=current,
-        tables = [{'name': 'runner-list'}],
+        tables=[{'name': 'time-trial-list'}],
         breadcrumbs=breadcrumbs,
     )
 
@@ -189,6 +190,24 @@ def time_trial_result(date):
     )
 
 
+@app.route('/time_trial/delete/<date>', methods=['GET', 'POST'])
+def time_trial_delete(date):
+    current_model = TimeTrial.query.filter_by(date=date).first_or_404()
+    db.session.delete(current_model)
+    db.session.commit()
+    flash('Your changes have been saved.')
+    return redirect(url_for('time_trial'))
+
+
+@app.route('/time_trial_result/delete/<id>', methods=['GET', 'POST'])
+def time_trial_result_delete(id):
+    current_model = TimeTrialResult.query.filter_by(id=id).first_or_404()
+    db.session.delete(current_model)
+    db.session.commit()
+    flash('Your changes have been saved.')
+    return redirect(url_for('time_trial'))
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
@@ -215,9 +234,14 @@ def logout():
 def admin():
     if not is_admin():
         raise Forbidden
+    breadcrumbs = [
+        {'link': url_for('index'), 'text': 'Home', 'visible': True},
+        {'text': 'Admin'}
+    ]
     return render_template(
         'admin/admin.html',
         title="Admin",
+        breadcrumbs=breadcrumbs
     )
 
 
@@ -231,9 +255,15 @@ def delete_data():
     TimeTrial.query.delete()
     TimeTrialResult.query.delete()
     flash('Data Deleted')
+    breadcrumbs = [
+        {'link': url_for('index'), 'text': 'Home', 'visible': True},
+        {'link': url_for('admin'), 'text': 'Admin', 'visible': True},
+        {'text': 'Delete Data'}
+    ]
     return render_template(
         'admin/admin.html',
         title="Admin",
+        breadcrumbs=breadcrumbs
     )
 
 
@@ -263,7 +293,7 @@ def parse_spreadsheet():
             username = v['first_name']+'_'+v['last_name']
             username = username.replace(' ', '_').lower()
             level = 1
-            if username == 'tv_' or username == 'daniel_jitnah' :
+            if username == 'tv_' or username == 'daniel_jitnah':
                 level = 2
             elif username == 'rosemary_waghorn':
                 level = 3
@@ -286,7 +316,7 @@ def parse_spreadsheet():
         results = sheet.get_time_trials_results_from()
         errors = []
         for v in results:
-            current_runner = Runner.query.filter_by(first_name=v['first_name'],last_name=v['last_name']).first()
+            current_runner = Runner.query.filter_by(first_name=v['first_name'], last_name=v['last_name']).first()
             current_time_trial = TimeTrial.query.filter_by(date=v['time_trial_date'].date()).first()
 
             result = TimeTrialResult()
@@ -300,12 +330,17 @@ def parse_spreadsheet():
             + str(len(data_trials)) + " time trial records added, " \
             + str(len(results)) + " time trial result records added" \
             + ', '.join(errors)
-
+    breadcrumbs = [
+        {'link': url_for('index'), 'text': 'Home', 'visible': True},
+        {'link': url_for('admin'), 'text': 'Admin', 'visible': True},
+        {'text': 'Parse Spreadsheet'}
+    ]
     return render_template(
         'admin/spreadsheet.html',
         title="Parse Spreadsheet",
         form=form,
-        message=response
+        message=response,
+        breadcrumbs=breadcrumbs
     )
 
 
@@ -329,7 +364,7 @@ def parse_attending():
         names = TimeTrialUtils.make_active(path)
         missing = []
         for v in names:
-            item = Runner.query.filter_by(first_name=v['first_name'],last_name=v['last_name']).first()
+            item = Runner.query.filter_by(first_name=v['first_name'], last_name=v['last_name']).first()
             if item:
                 item.active = 1
             else:
@@ -340,12 +375,17 @@ def parse_attending():
         message += str(missing)
 
         flash('Attendance file uploaded')
-
+    breadcrumbs = [
+        {'link': url_for('index'), 'text': 'Home', 'visible': True},
+        {'link': url_for('admin'), 'text': 'Admin', 'visible': True},
+        {'text': 'Parse Attending'}
+    ]
     return render_template(
         'admin/attending.html',
         title="Parse Attending",
         form=form,
-        message=message
+        message=message,
+        breadcrumbs=breadcrumbs
     )
 
 
@@ -367,7 +407,7 @@ def create_printed_timesheet():
             for k in Runner.query.order_by(Runner.first_name.asc()).filter_by(active=1)
         ]
 
-        template = sheet.get_template_from(active_runners, current_time_trial.date, path)
+        sheet.get_template_from(active_runners, current_time_trial.date, path)
 
         return send_file(
             path_read,
@@ -375,11 +415,16 @@ def create_printed_timesheet():
             as_attachment=True,
             attachment_filename=filename
         )
-
+    breadcrumbs = [
+        {'link': url_for('index'), 'text': 'Home', 'visible': True},
+        {'link': url_for('admin'), 'text': 'Admin', 'visible': True},
+        {'text': 'Print TimeSheet'}
+    ]
     return render_template(
         'admin/print_timesheet.html',
         title="Print TimeSheet",
         form=form,
+        breadcrumbs=breadcrumbs
     )
 
 
@@ -408,10 +453,16 @@ def export_results():
             as_attachment=True,
             attachment_filename=filename
         )
+    breadcrumbs = [
+        {'link': url_for('index'), 'text': 'Home', 'visible': True},
+        {'link': url_for('admin'), 'text': 'Admin', 'visible': True},
+        {'text': 'Export Results'}
+    ]
     return render_template(
         'admin/export_results.html',
         title="Export Results",
         form=form,
+        breadcrumbs=breadcrumbs
     )
 
 
@@ -426,22 +477,22 @@ def is_sys_admin():
 def make_graph(username, results):
     times = [v.time for v in results]
     time_trial_dates = [v.time_trial.date.strftime('%b %Y') for v in results]
-    #for item in results:
+    # for item in results:
     #    times.append(item/)
-    #lnprice = numpy.log(times)
+    # lnprice = numpy.log(times)
     # .strftime('%M:%S')
-    ax = plt.plot(time_trial_dates, times)
+    # ax = plt.plot(time_trial_dates, times)
+    plt.plot(time_trial_dates, times)
     plt.xticks(rotation=30)
     plt.xlabel('Time Trial')
     plt.ylabel('Time')
-    #plt.gca().yaxis.set_major_formatter(dates2.DateFormatter('%M:%S'))
-    #plt.gca().yaxis.set_major_formatter(ticker.FuncFormatter(major_formatter))
-    #plt.gca().yaxis.set_major_formatter( mdates.DateFormatter('%Y-%m-%d'))
+    # plt.gca().yaxis.set_major_formatter(dates2.DateFormatter('%M:%S'))
+    # plt.gca().yaxis.set_major_formatter(ticker.FuncFormatter(major_formatter))
+    # plt.gca().yaxis.set_major_formatter( mdates.DateFormatter('%Y-%m-%d'))
 
-
-    #plt.axis(dates)
-    #plt.axes()
-    #plt.set_formatter(dates.DateFormatter('%H:%M'))
+    # plt.axis(dates)
+    # plt.axes()
+    # plt.set_formatter(dates.DateFormatter('%H:%M'))
     filename = 'images/runner_'+username+'.png'
     path = 'app/static/' + filename
     plt.savefig(path)
@@ -449,7 +500,7 @@ def make_graph(username, results):
     return url_for('static', filename=filename)
 
 
-# Func formatter
-def major_formatter(x, pos):
-    #time = datetime64
-    return type(x) #x.strftime('%M:%S')
+# # Func formatter
+# def major_formatter(x, pos):
+#     # time = datetime64
+#     return type(x) # x.strftime('%M:%S')
